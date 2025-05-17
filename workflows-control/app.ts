c48afb3f483961b8557e2832a827fc2fbbc61ee7
chenchugokuljangam13 <jangamchenchugokul@gmail.com>
2025-05-18 01:45:54 +0530

    updated code

--- workflows-control/app.ts
+++ workflows-control/app.ts
@@ -3,19 +3,22 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
     try {
         const body = JSON.parse(event.body!);
         const decodedLog = JSON.parse(Buffer.from(body.log, 'base64').toString('utf-8'));
-        console.log("repo name",body.repository)
-        console.log("conclusion", body.conclusion)
-        console.log("status", body.status)
-        console.log("encodedLog", body.log)
-        console.log("decodedLog", decodedLog)
+        console.log("Repo name:",body.repository);
+        console.log("Conclusion:", body.conclusion);
+        console.log("Status:", body.status);
+        console.log("Total test cases: ", decodedLog.numTotalTests);
+        console.log("Test cases passed: ", decodedLog.numPassedTests);
+        console.log("Test cases failed: ", decodedLog.numFailedTests);
+        console.log("Encoded Log: ", body.log);
+        console.log(decodedLog);
          const testHistory = [];
         for (let x of decodedLog?.testResults[0]?.assertionResults) {
                 const val = {
                     testName: x.title,
                     testStatus: x.status
-                }
+                };
                 testHistory.push(val)
-        }
+        };
         console.log(testHistory)
         return {
             statusCode: 200,
